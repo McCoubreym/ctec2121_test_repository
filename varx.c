b46a6d68ca14d909d1418328561e8c608b7e482a
P16190339 <P16190339@my365.dmu.ac.uk>
2018-01-16 14:50:29 +0000

    Added a c file where it will add, subtract, multiply, divide

---
+++ varx.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 30;
+    int y = 5;
+
+    printf ("%i, this is the value of x\n", x+y);
+    printf ("%i, this is the value of x\n", x-y);
+    printf ("%i, this is the value of x\n", x*y);
+    printf ("%i, this is the value of x\n", x/y);
+    return 0;
+}
